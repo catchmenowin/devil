eefc8d192d168064a505088037b918fff47df0e7
catchme <catchmenowin@gmail.com>
2014-05-06 12:05:23 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devil::Application.config.secret_key_base = 'eeac82c73ed1e08c0767b360fc5474abefdaf1f7cd8f2f9dc1e17c3dc33d88ae2ad6193ee959fcd00a60a4a300339938d9a353dd57f2155821768ea5b61d3066'
